ee89b0da6f46507ae316097a850a754d636dff1b
TranVanTienDat <ckim32993@gmail.com>
2023-09-14 10:25:05 +0700

    first commit

---
+++ components/index.tsx
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import Button from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  Button,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  ShowMore,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
